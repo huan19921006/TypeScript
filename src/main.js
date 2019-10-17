4899d98619997c8e8eaec93a3b1f6084c8db3744
huan19921006 <526382350@qq.com>
2019-10-17 10:17:03 +0800

    te

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -10,7 +9,7 @@ Vue.use(Vuex);
 Vue.use(VueBus);
 Vue.config.productionTip = false
 
-//vueX 用法
+//vueX 用法456
 const store = new Vuex.Store({
   state: {
     count: 0
