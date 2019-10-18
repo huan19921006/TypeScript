9969f92d0bb33ae2760b11b7930a8803d449b00c
huan19921006 <526382350@qq.com>
2019-10-18 11:32:20 +0800

    1

--- test.js
+++ test.js
@@ -5,3 +5,25 @@ greetUnnamed = function (name) {
     }
 };
 alert(greetUnnamed('huan'));
+function add() {
+    var foo = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        foo[_i] = arguments[_i];
+    }
+    var result = 0;
+    for (var i = 0; i < foo.length; i++) {
+        result += foo[i];
+    }
+    return result;
+}
+alert(add(2, 2));
+alert(add(2, 2, 2));
+var bar = 0;
+(function (global) {
+    var foo = 0;
+    bar = 1;
+    console.log(global.bar);
+    console.log(foo);
+})(this);
+console.log(bar);
+
--- test.ts
+++ test.ts
@@ -5,3 +5,25 @@ greetUnnamed = function (name: string) : string {
     }
 }
 alert(greetUnnamed('huan'));
+
+function add (...foo: number[]): number {
+    var result = 0;
+    for (var i = 0; i < foo.length; i++) {
+        result += foo[i];
+    }
+    return result;
+}
+
+alert(add(2,2));
+alert(add(2,2,2));
+
+var bar = 0;
+
+(function(global) {
+    var foo: number = 0;
+    bar = 1;
+    console.log(global.bar);
+    console.log(foo);
+})(this);
+
+console.log(bar);
